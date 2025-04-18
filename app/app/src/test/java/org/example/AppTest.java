ca93744151b3f499f0585f768e61a3e6ffdf0e73
k2works <kakimomokuri@gmail.com>
2025-04-18 02:57:32 +0000

    test:仮実装

--- app/app/src/test/java/org/example/AppTest.java
+++ app/app/src/test/java/org/example/AppTest.java
@@ -3,12 +3,19 @@
  */
 package org.example;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    @DisplayName("0を渡したら0を返す")
+    @Test void when0_then0() {
+        assertEquals(0, Fibonacci.fib(0));
+    }
+}
+
+class Fibonacci {
+    public static int fib(int i) {
+        return 0;
     }
 }
