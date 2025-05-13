2d34190704b510d3605aac1ee57f29dbe3229ae8
k2works <kakimomokuri@gmail.com>
2025-05-13 10:42:15 +0900

    test:セットアップ

--- app/test/app/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns app.core-test
-  (:require [clojure.test :refer :all]
-            [app.core :as core]))
-
-(deftest hello-world-test
-  (testing "hello-world function returns the expected greeting"
-    (is (= "Hello, World!" (core/hello-world)))))
