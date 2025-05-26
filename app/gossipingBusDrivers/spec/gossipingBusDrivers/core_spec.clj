feb3e2d5b99e5094dace6755b0b58f2a44a4c2d3
k2works <kakimomokuri@gmail.com>
2025-05-26 12:31:43 +0900

    test:セットアップ

---
+++ app/gossipingBusDrivers/spec/gossipingBusDrivers/core_spec.clj
@@ -0,0 +1,5 @@
+(ns gossipingBusDrivers.core-spec
+  (:require [speclj.core :refer :all]
+            [gossipingBusDrivers.core :refer :all]))
+
+(describe "Gossiping Bus Drivers")
---
+++ app/gossipingBusDrivers/spec/gossipingBusDrivers/core_test.clj
@@ -0,0 +1,5 @@
+(ns gossipingBusDrivers.core-test
+  (:require [clojure.test :refer :all]
+            [gossipingBusDrivers.core :refer :all]))
+
+(deftest gossipingBusDrivers-test)
