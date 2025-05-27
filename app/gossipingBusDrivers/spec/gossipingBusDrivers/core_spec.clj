75c18cb45448c8937ca82810b143f51ff2eb04d3
k2works <kakimomokuri@gmail.com>
2025-05-27 11:40:19 +0900

    test:ゴシップ好きなバスの運転手

--- app/gossipingBusDrivers/spec/gossipingBusDrivers/core_spec.clj
+++ app/gossipingBusDrivers/spec/gossipingBusDrivers/core_spec.clj
@@ -8,4 +8,10 @@
                     world [driver]
                     new-world (drive world)]
                 (should= 1 (count new-world))
-                (should= :s1 (-> new-world first :route first)))))
+                (should= :s1 (-> new-world first :route first))))
+          (it "drive one bus at two stops"
+              (let [driver (make-driver "d1" [:s1 :s2] #{:r1})
+                    world [driver]
+                    new-world (drive world)]
+                (should= 1 (count new-world))
+                (should= :s2 (-> new-world first :route first)))))
--- app/gossipingBusDrivers/spec/gossipingBusDrivers/core_test.clj
+++ app/gossipingBusDrivers/spec/gossipingBusDrivers/core_test.clj
@@ -3,9 +3,15 @@
             [gossipingBusDrivers.core :refer :all]))
 
 (deftest gossipingBusDrivers-test
-  (testing "Gossiping Bus Drivers")
+  (testing "drives one bus at one stop")
     (let [driver (make-driver "d1" [:s1] #{:r1})
           world [driver]
           new-world (drive world)]
       (is (= 1 (count new-world)))
-      (is (= :s1 (-> new-world first :route first)))))
+      (is (= :s1 (-> new-world first :route first))))
+  (testing "drives one bus at two stops")
+  (let [driver (make-driver "d1" [:s1 :s2] #{:r1})
+          world [driver]
+          new-world (drive world)]
+      (is (= 1 (count new-world)))
+      (is (= :s2 (-> new-world first :route first)))))
