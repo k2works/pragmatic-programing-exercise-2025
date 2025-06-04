7a7ae4dbab99e509e28e7f2be54d59752c76370f
k2works <kakimomokuri@gmail.com>
2025-06-04 12:40:54 +0900

    test:給与計算

--- app/payroll/src/payroll/core.clj
+++ app/payroll/src/payroll/core.clj
@@ -13,124 +13,57 @@
   (first (:pay-class employee)))
 
 (defn get-disposition [paycheck-directive]
-  (first (:disposition paycheck-directive)))
-(defn _get-dispositions [employees]
   "Extract the disposition of each employee"
-  (map :disposition employees))
+  (first (:disposition paycheck-directive)))
 
 (defmulti is-tody-payday :schedule)
 (defmulti calc-pay get-pay-class)
 (defmulti dispose get-disposition)
 
 (defn get-employees-to-be-paid-today [today employees]
-  (filter #(is-tody-payday % today) employees))
-(defn _get-employees-to-be-paid-today [tody employees]
   "Check the schedule of each employee based on the date"
-  (cond
-    (= tody (LocalDate/of 2021 11 30))
-    (filter #(= (:schedule %) :monthly) employees)
-
-    (= tody (LocalDate/of 2022 11 18))
-    (concat
-      (filter #(= (:schedule %) :weekly) employees)
-      (filter #(= (:schedule %) :biweekly) employees))
-
-    :else []))
+  (filter #(is-tody-payday % today) employees))
 
 (defn- build-employee [db employee]
   (assoc employee :db db))
 
 (defn get-employees [db]
   (map (partial build-employee db) (:employees db)))
-(defn _get-employees [db]
-  (:employees db))
 
 (defn create-paycheck-directive [ids payments dispositions]
   (map #(assoc {} :id %1 :amount %2 :disposition %3)
        ids payments dispositions))
 
 (defn send-paychecks [ids payments dispositions]
+  "Create a paycheck for each employee"
   (for [paycheck-directive
         (create-paycheck-directive ids payments dispositions)]
     (dispose paycheck-directive)))
-(defn _send-paychecks [ids amounts dispositions]
-  "Create a paycheck for each employee"
-  (map (fn [id amount disposition]
-         (let [disp-type (first disposition)]
-           (case disp-type
-             :mail (let [name (second disposition)
-                         address (nth disposition 2)]
-                     {:type :mail
-                      :id id
-                      :name name
-                      :address address
-                      :amount amount})
-             :deposit (let [routing (second disposition)
-                            account (nth disposition 2)]
-                        {:type :deposit
-                         :id id
-                         :routing routing
-                         :account account
-                         :amount amount})
-             :paymaster (let [paymaster (second disposition)]
-                          {:type :paymaster
-                           :id id
-                           :paymaster paymaster
-                           :amount amount}))))
-       ids amounts dispositions))
 
 (defn get-paycheck-amounts [employees]
-  (map calc-pay employees))
-(defn _get-paycheck-amounts [employees db]
   "Calculate the amount based on the pay-class of each employee"
-  (map (fn [employee]
-         (let [pay-class (:pay-class employee)
-               pay-type (first pay-class)
-               id (:id employee)]
-           (case pay-type
-             :salaried (second pay-class)
-             :hourly (let [time-cards (get-in db [:time-cards id])
-                           hours (if time-cards
-                                   (second (first time-cards))
-                                   0)
-                           hourly-rate (second pay-class)]
-                       (* hours hourly-rate))
-             :commissioned (let [base-salary (second pay-class)
-                                 commission-rate (nth pay-class 2)
-                                 sales-receipts (get-in db [:sales-receipts id])
-                                 sales-amount (if sales-receipts
-                                                (second (first sales-receipts))
-                                                0)]
-                             (+ base-salary (* sales-amount commission-rate))))))
-       employees))
-
+  (map calc-pay employees))
 
 (defn get-dispositions [employees]
   (map :disposition employees))
 
 (defn get-ids [employees]
-  (map :id employees))
-(defn _get-ids [employees]
   "Extract the id of each employee"
   (map :id employees))
 
-;; 月給制の社員は月末に支払い
 (defmethod is-tody-payday :monthly [_ today]
   (let [last-day-of-month (.lengthOfMonth today)
         day-of-month (.getDayOfMonth today)]
     (= day-of-month last-day-of-month)))
 
-;; 週給制の社員は毎週金曜日に支払い
 (defmethod is-tody-payday :weekly [_ today]
   (= (.getValue (.getDayOfWeek today)) 5)) ;; 5は金曜日
 
-;; 隔週給制の社員は隔週金曜日に支払い
 (defmethod is-tody-payday :biweekly [_ today]
   (and
     (= (.getValue (.getDayOfWeek today)) 5) ;; 金曜日
     (even? (quot (.getDayOfYear today) 7)))) ;; 偶数週
 
-;; 郵送による給与支払い
 (defmethod dispose :mail [paycheck-directive]
   (let [id (:id paycheck-directive)
         amount (:amount paycheck-directive)
@@ -142,7 +75,6 @@
      :address address
      :amount amount}))
 
-;; 銀行振込による給与支払い
 (defmethod dispose :deposit [paycheck-directive]
   (let [id (:id paycheck-directive)
         amount (:amount paycheck-directive)
@@ -154,7 +86,6 @@
      :account account
      :amount amount}))
 
-;; 給与管理者経由の給与支払い
 (defmethod dispose :paymaster [paycheck-directive]
   (let [id (:id paycheck-directive)
         amount (:amount paycheck-directive)
@@ -163,33 +94,30 @@
      :id id
      :paymaster paymaster
      :amount amount}))
-;; 固定給の社員の給与計算
+
+(defn- get-salary [employee]
+  (second (:pay-class employee)))
+
 (defmethod calc-pay :salaried [employee]
-  (let [salary (second (:pay-class employee))]
-    salary))
+  (get-salary employee))
 
-;; 時給制の社員の給与計算
 (defmethod calc-pay :hourly [employee]
-  (let [hourly-rate (second (:pay-class employee))
-        db (:db employee)
-        id (:id employee)
-        time-cards (get-in db [:time-cards id])
-        hours (if time-cards
-                (second (first time-cards))
-                0)]
-    (* hours hourly-rate)))
+  (let [db (:db employee)
+        time-cards (:time-cards db)
+        my-time-cards (get time-cards (:id employee))
+        [_ hourly-rate] (:pay-class employee)
+        hours (map second my-time-cards)
+        total-hours (reduce + hours)]
+    (* total-hours hourly-rate)))
 
-;; 歩合制の社員の給与計算
 (defmethod calc-pay :commissioned [employee]
-  (let [base-salary (second (:pay-class employee))
-        commission-rate (nth (:pay-class employee) 2)
-        db (:db employee)
-        id (:id employee)
-        sales-receipts (get-in db [:sales-receipts id])
-        sales-amount (if sales-receipts
-                       (second (first sales-receipts))
-                       0)]
-    (+ base-salary (* sales-amount commission-rate))))
+  (let [db (:db employee)
+        sales-receipts (:sales-receipts db)
+        my-sales-receipts (get sales-receipts (:id employee))
+        [_ base-pay commission-rate] (:pay-class employee)
+        sales (map second my-sales-receipts)
+        total-sales (reduce + sales)]
+    (+ (* total-sales commission-rate) base-pay)))
 
 (defn payroll [tody db]
   (let [employees (get-employees db)
