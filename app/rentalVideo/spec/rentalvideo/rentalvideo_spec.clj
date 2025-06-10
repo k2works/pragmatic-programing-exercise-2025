43b49d7fa4e6ee8d75677e1aee67dd320c75b253
k2works <kakimomokuri@gmail.com>
2025-06-10 11:15:35 +0900

    test:レンタルビデオ

--- app/rentalVideo/spec/rentalvideo/rentalvideo_spec.clj
+++ app/rentalVideo/spec/rentalvideo/rentalvideo_spec.clj
@@ -31,7 +31,7 @@
                             (make-rental
                               (make-movie "The Tigger Movie" :new-release)
                               3)]))))
-          (it "make statement for one childrens movie"
+          (it "makes statement for one childrens movie"
               (should= (str "Rental Record for Fred\n"
                             "\tThe Tigger Movie\t1.5\n"
                             "You owed 1.5\n"
@@ -41,4 +41,23 @@
                            @customer
                            [(make-rental
                               (make-movie "The Tigger Movie" :children)
+                              3)]))))
+          (it "makes statement for several regular movies"
+              (should= (str "Rental Record for Fred\n"
+                            "\tPlan 9 from Outer Space\t2.0\n"
+                            "\t8 1/2\t2.0\n"
+                            "\tEraserhead\t3.5\n"
+                            "You owed 7.5\n"
+                            "You earned 3 frequent renter points\n")
+                       (make-statement
+                         (make-rental-order
+                           @customer
+                           [(make-rental
+                              (make-movie "Plan 9 from Outer Space" :regular)
+                              1)
+                            (make-rental
+                              (make-movie "8 1/2" :regular)
+                              2)
+                            (make-rental
+                              (make-movie "Eraserhead" :regular)
                               3)])))))
--- app/rentalVideo/spec/rentalvideo/rentalvideo_test.clj
+++ app/rentalVideo/spec/rentalvideo/rentalvideo_test.clj
@@ -11,7 +11,7 @@
                   "You earned 2 frequent renter points\n")
              (make-statement
                (make-rental-order
-                 customer  ; @を削除
+                 customer
                  [(make-rental
                     (make-movie "The Cell" :new-release)
                     3)]))))))
@@ -44,4 +44,25 @@
                  customer
                  [(make-rental
                     (make-movie "The Tigger Movie" :children)
-                    3)])))))))
+                    3)]))))))
+
+  (testing "makes statement for several regular movies"
+    (let [customer (make-customer "Fred")]
+      (is (= (str "Rental Record for Fred\n"
+                    "\tPlan 9 from Outer Space\t2.0\n"
+                    "\t8 1/2\t2.0\n"
+                    "\tEraserhead\t3.5\n"
+                    "You owed 7.5\n"
+                    "You earned 3 frequent renter points\n")
+               (make-statement
+                 (make-rental-order
+                   customer
+                   [(make-rental
+                      (make-movie "Plan 9 from Outer Space" :regular)
+                      1)
+                    (make-rental
+                      (make-movie "8 1/2" :regular)
+                      2)
+                    (make-rental
+                      (make-movie "Eraserhead" :regular)
+                      3)])))))))
