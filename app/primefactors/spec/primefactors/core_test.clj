77192d68a65b4d1a32c8a2fe9b72dc0fa30f0536
k2works <kakimomokuri@gmail.com>
2025-06-19 11:47:57 +0900

    test:素因数分解

--- app/primefactors/spec/primefactors/core_test.clj
+++ app/primefactors/spec/primefactors/core_test.clj
@@ -1,6 +1,9 @@
 (ns primefactors.core-test
   (:require [clojure.test :refer [deftest is testing]]
-            [primefactors.core :refer [prime-factors-of]]))
+            [clojure.test.check :as tc]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.generators :as gen-tc]
+            [primefactors.core :refer :all]))
 
 (deftest prime-factors-test
   (testing "returns [] for 1"
@@ -24,3 +27,60 @@
   (testing "returns [5 5] for 25"
     (is (= [5 5] (prime-factors-of 25))))
   )
+
+(defn power2 [n]
+  (apply * (repeat n 2N)))
+
+(deftest factors-test
+  (testing "factors 1 -> []"
+    (is (= [] (prime-factors-of 1))))
+  (testing "factors 2 -> [2]"
+    (is (= [2] (prime-factors-of 2))))
+  (testing "factors 3 -> [3]"
+    (is (= [3] (prime-factors-of 3))))
+  (testing "factors 4 -> [2 2]"
+    (is (= [2 2] (prime-factors-of 4))))
+  (testing "factors 5 -> [5]"
+    (is (= [5] (prime-factors-of 5))))
+  (testing "factors 6 -> [2 3]"
+    (is (= [2 3] (prime-factors-of 6))))
+  (testing "factors 7 -> [7]"
+    (is (= [7] (prime-factors-of 7))))
+  (testing "factors 8 -> [2 2 2]"
+    (is (= [2 2 2] (prime-factors-of 8))))
+  (testing "factors 9 -> [3 3]"
+    (is (= [3 3] (prime-factors-of 9))))
+  (testing "factors lots"
+    (is (= [2 2 3 3 5 7 11 11 13]
+           (prime-factors-of (* 2 2 3 3 5 7 11 11 13)))))
+
+  (testing "factors Euler 3"
+    (is (= [71 839 1471 6857] (prime-factors-of 600851475143))))
+
+  (testing "factors mersenne 2^31-1"
+    (is (= [2147483647] (prime-factors-of (dec (power2 31)))))))
+
+(def gen-inputs (gen-tc/large-integer* {:min 1 :max 1E9}))
+
+(deftest factors-multiply-out-property-test
+  (testing "factors multiply out properly (generative)"
+    (let [result (tc/quick-check
+                   1000
+                   (prop/for-all
+                     [n gen-inputs]
+                     (let [factors (factors-of n)]
+                       (= n (apply * factors)))))]
+      (is (:result result)
+          (str "Property test failed: " (:fail result))))))
+
+(deftest factors-are-prime-property-test
+  (testing "all factors are prime (generative)"
+    (let [result (tc/quick-check
+                   1000
+                   (prop/for-all
+                     [n gen-inputs]
+                     (let [factors (factors-of n)]
+                       (every? is-prime? factors))))]
+      (is (:result result)
+          (str "Property test failed: " (:fail result))))))
+
--- app/primefactors/src/primefactors/core.clj
+++ app/primefactors/src/primefactors/core.clj
@@ -7,3 +7,27 @@
         (recur (quot n divisor) divisor (conj factors divisor))
         (recur n (inc divisor) factors))
       factors)))
+
+(defn factors-of [n]
+  (loop [factors [] n n divisor 2]
+    (if (> n 1)
+      (cond
+        (> divisor (Math/sqrt n))
+        (conj factors n)
+        (= 0 (mod n divisor))
+        (recur (conj factors divisor)
+               (quot n divisor)
+               divisor)
+        :else
+        (recur factors n (inc divisor)))
+      factors)))
+
+(defn is-prime? [n]
+  (if (= 2 n)
+    true
+    (loop [candidates (range 2 (inc (Math/sqrt n)))]
+      (if (empty? candidates)
+        true
+        (if (zero? (rem n (first candidates)))
+          false
+          (recur (rest candidates)))))))
