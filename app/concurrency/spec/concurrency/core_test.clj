1d649fde60a19bbb26783d54f7a89c3b16d60b17
k2works <kakimomokuri@gmail.com>
2025-07-01 12:26:43 +0900

    test:並行性

--- app/concurrency/spec/concurrency/core_test.clj
+++ app/concurrency/spec/concurrency/core_test.clj
@@ -14,3 +14,19 @@
       (is (= :idle (:state @caller)))
       (is (= :idle (:state @callee)))
       (is (= :idle (:state @telco))))))
+
+(deftest race-test
+  (testing "should race"
+    (let [caller (make-user "Bob")
+          callee (make-user "Alice")
+          telco1 (make-teloc "telco1")
+          telco2 (make-teloc "telco2")]
+      (reset! log [])
+      (send caller transition :call [telco1 caller callee])
+      (send caller transition :call [telco2 callee caller])
+      (Thread/sleep 100)
+      (prn @log)
+      (is (= :idle (:state @caller)))
+      (is (= :idle (:state @callee)))
+      (is (= :idle (:state @telco1)))
+      (is (= :idle (:state @telco2))))))
