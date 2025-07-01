4b5365b95ff335ebbf83b124cb3d9bdc0304d865
k2works <kakimomokuri@gmail.com>
2025-07-01 11:56:23 +0900

    test:並行性

--- app/concurrency/spec/concurrency/core_spec.clj
+++ app/concurrency/spec/concurrency/core_spec.clj
@@ -1,3 +1,16 @@
 (ns concurrency.core-spec
   (:require [concurrency.core :refer :all]
             [speclj.core :refer :all]))
+
+(describe "Concurrency"
+          (it "should make and receive call"
+              (let [caller (make-user "Bob")
+                    callee (make-user "Alice")
+                    telco (make-teloc "telco")]
+                (reset! log [])
+                (send caller transition :call [telco caller callee])
+                (Thread/sleep 100)
+                (prn @log)
+                (should= :idle (:state @caller))
+                (should= :idle (:state @callee))
+                (should= :idle (:state @telco)))))
--- app/concurrency/spec/concurrency/core_test.clj
+++ app/concurrency/spec/concurrency/core_test.clj
@@ -2,3 +2,15 @@
   (:require [clojure.test :refer :all]
             [concurrency.core :refer :all]))
 
+(deftest concurrency-test
+  (testing "should make and receive call"
+    (let [caller (make-user "Bob")
+          callee (make-user "Alice")
+          telco (make-teloc "telco")]
+      (reset! log [])
+      (send caller transition :call [telco caller callee])
+      (Thread/sleep 100)
+      (prn @log)
+      (is (= :idle (:state @caller)))
+      (is (= :idle (:state @callee)))
+      (is (= :idle (:state @telco))))))
