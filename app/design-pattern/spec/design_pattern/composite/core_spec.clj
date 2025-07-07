75b2fe47886afd2f6d09303211b94c760d15beba
k2works <kakimomokuri@gmail.com>
2025-07-07 11:29:03 +0900

    test: composite

--- app/design-pattern/spec/design_pattern/composite/core_spec.clj
+++ app/design-pattern/spec/design_pattern/composite/core_spec.clj
@@ -2,7 +2,8 @@
   (:require [speclj.core :refer :all]
             [design-pattern.composite [light :as l]
              [variable-light :as v]
-             [switchable :as s]]))
+             [switchable :as s]
+             [composite-switchable :as cs]]))
 
 (describe "composite-switchable"
           (with-stubs)
@@ -12,6 +13,19 @@
                  v/set-light-intensity (stub :set-light-intensity)]
                 (let [switchables [(l/make-light) (v/make-variable-light)]]
                   (doseq [s-able switchables] (s/turn-on s-able))
+                  (should-have-invoked :turn-on-light)
+                  (should-have-invoked :set-light-intensity
+                                       {:with [100]})))))
+(describe "composite-switchable"
+          (with-stubs)
+          (it "turns all on"
+              (with-redefs
+                [l/turn-on-light (stub :turn-on-light)
+                 v/set-light-intensity (stub :set-light-intensity)]
+                (let [group (-> (cs/make-composite-switchable)
+                                (cs/add (l/make-light))
+                                (cs/add (v/make-variable-light)))]
+                  (s/turn-on group)
                   (should-have-invoked :turn-on-light)
                   (should-have-invoked :set-light-intensity
                                        {:with [100]})))))
