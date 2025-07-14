1700a5d3252ce09f3f33f36e60dbc86570e9905a
k2works <kakimomokuri@gmail.com>
2025-07-14 13:26:16 +0900

    feat: add water evolution logic and corresponding tests

--- app/wa-tor/spec/wator/core_test.clj
+++ app/wa-tor/spec/wator/core_test.clj
@@ -1,3 +1,20 @@
 (ns wator.core-test
   (:require [clojure.test :refer :all]
-            [wator.core :refer :all]))
+            [wator.core :refer :all]
+            [wator
+             [cell :as cell]
+             [water :as water]
+             [fish :as fish]]))
+
+(deftest water-test
+  (testing "water usually remains water"
+    (with-redefs [rand (constantly 0.0)]
+      (let [water (water/make)
+            evolved (cell/tick water)]
+        (is (= ::water/water (::cell/type evolved))))))
+
+  (testing "water occasionally evolves into a fish"
+    (with-redefs [rand (constantly 1.0)]
+      (let [water (water/make)
+            evolved (cell/tick water)]
+        (is (= ::fish/fish (::cell/type evolved)))))))
